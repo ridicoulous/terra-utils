d361e148a7f58c392c042bd94bb8ae0d14bd63c1
ridicoulous <artemkuryanov@gmail.com>
2021-12-13 22:11:14 +0200

    code cleanup

--- tx-decoder.go
+++ tx-decoder.go
@@ -12,9 +12,6 @@ import (
 	"github.com/tendermint/tendermint/crypto"
 	marketTypes "github.com/terra-money/core/x/market/types"
 	oracleTypes "github.com/terra-money/core/x/oracle/types"
-
-	// legacyTypes "github.com/terra-money/core/x/msgauth/legacy/v04"
-	//	treasuryTypes "github.com/terra-money/core/x/treasury/types"
 	vestingTypes "github.com/terra-money/core/x/vesting/types"
 	wasmTypes "github.com/terra-money/core/x/wasm/types"
 )
@@ -31,7 +28,6 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	// Handle the error and work with tx
 	txJson, err := tx.MarshalToJSON()
 	if err != nil {
 		fmt.Println(err)
@@ -44,9 +40,6 @@ func RegisterDefaultInterfaces(interfaceRegistry types.InterfaceRegistry) {
 	std.RegisterInterfaces(interfaceRegistry)
 	oracleTypes.RegisterInterfaces(interfaceRegistry)
 	marketTypes.RegisterInterfaces(interfaceRegistry)
-	// legacyTypes.RegisterInterfaces(interfaceRegistry)
-	//	treasuryTypes.RegisterInterfaces(interfaceRegistry)
 	vestingTypes.RegisterInterfaces(interfaceRegistry)
 	wasmTypes.RegisterInterfaces(interfaceRegistry)
-
 }
